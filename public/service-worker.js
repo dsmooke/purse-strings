25701559ae58e1c3ee8383633d47541cf34c3efb
Danny Smooke <dana.smooke@gmail.com>
2021-03-16 13:27:42 -0400

    added db file and edited style file, fixed errors, cleaned up comments - GREAT

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,9 @@
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
-  "/style.css",
+  "/styles.css",
   "/index.js",
+  "/db.js",
   "/manifest.webmanifest",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
@@ -11,29 +12,39 @@ const FILES_TO_CACHE = [
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-// Install and register your service worker
+// Install
 self.addEventListener("install", function (evt) {
+  // pre cache data
   evt.waitUntil(
-    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
+    caches.open(DATA_CACHE_NAME).then((cache) => {
+      console.log("Your (data) was pre-cached!");
+      return cache.add("/api/transaction");
+    })
   );
 
-  // Pre cache all static assets
-  /* evt.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))); 
-          console.log("Your files were pre-cached successfully!");
-    */
+  // pre cache all static assets
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Your static files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 
-  // Tell the browser to activate this service worker immediately once it has finished installing
+  // tell the browser to activate this service worker immediately once it has finished installing
   self.skipWaiting();
 });
 
-// Activate the service worker and remove old data from the cache
+// *** Done successfully, should see static cache in Application tab in devtools
+
+// Activate
 self.addEventListener("activate", function (evt) {
+  // activating service worker
   evt.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(
         keyList.map((key) => {
-          if (key !== DATA_CACHE_NAME) {
+          // removing old data from the cache
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
             console.log("Removing old cache data", key);
             return caches.delete(key);
           }
@@ -45,39 +56,34 @@ self.addEventListener("activate", function (evt) {
   self.clients.claim();
 });
 
-// Fetch/ Enable service worker to intercept network requests
+// Fetch
 self.addEventListener("fetch", function (evt) {
-  // code to handle the request
+  // cache responses for requests for data
 
   if (evt.request.url.includes("/api/")) {
     console.log("[Service Worker] Fetch (data)", evt.request.url);
 
     evt.respondWith(
-      caches
-        .open(DATA_CACHE_NAME)
-        .then((cache) => {
-          return fetch(evt.request)
-            .then((response) => {
-              // if response good, clone it and store it in cache
+      caches.open(DATA_CACHE_NAME).then((cache) => {
+        return fetch(evt.request)
+          .then((response) => {
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
 
-              if (response.status === 200) {
-                cache.put(evt.request.url, response.clone());
-              }
-
-              return response;
-            })
-            .catch((err) => {
-              return cache.match(evt.request);
-            });
-        })
-        .catch((err) => console.log(err))
+            return response;
+          })
+          .catch((err) => {
+            return cache.match(evt.request);
+          });
+      })
     );
     return;
   }
 
-  // Serve static files from the cache, proceed with a network request when the resource is not in the cache - Allows the page to be accessible offline
+  // this code allows the page to be accessible offline
   evt.respondWith(
-    // @audit-issue "the FetchEvent for http://localhost:3000/ resulted in a network error response: the promise was rejected" Promise.then (async) (anonymous) at line 78
+    // serve static files from the cache; proceed with a network request when resource is not in cache
 
     caches.open(CACHE_NAME).then((cache) => {
       return cache.match(evt.request).then((response) => {
